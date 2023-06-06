5b7b919d47fa352dff11326c72db58a67de4442a
falsher-dev <74367284+falsher-dev@users.noreply.github.com>
2023-06-06 23:52:59 +0600

    Delete Laba Odin directory

--- Laba Odin/Laba 1 File 2.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-
-int main2() {
-	double R = 15;
-	const double PI = 3.14;
-	double S = PI * R * R;
-	std::cout << "Ploshad kruga s radiusom " << R << " ravna " << S;
-	return 0;
-}
